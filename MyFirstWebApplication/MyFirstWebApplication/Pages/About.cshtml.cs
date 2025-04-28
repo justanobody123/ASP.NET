9ea420accee30685410c7bf2b1442815ea2f02a8
Кристина Волосова <justanobody017527@gmail.com>
2025-04-28 13:40:58 +0300

    Add project files.

---
+++ MyFirstWebApplication/MyFirstWebApplication/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstWebApplication.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MyFirstWebApplication/MyFirstWebApplication/Pages/Addresses.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstWebApplication.Pages
+{
+    public class AddressesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
